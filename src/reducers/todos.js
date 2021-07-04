8df4b63acdfa2524d201d08545df39a5b1e325dc
Gal Uzan <galuzan180@gmail.com>
2021-07-04 17:37:21 +0300

    first commit

---
+++ src/reducers/todos.js
@@ -0,0 +1,99 @@
+import {
+  ADD_TODO,
+  DELETE_TODO,
+  EDIT_TODO,
+  COMPLETE_TODO,
+  COMPLETE_ALL_TODOS,
+  CLEAR_COMPLETED
+} from "../constants/ActionTypes";
+
+// const initialState = [
+//   {
+//     text: "Implement Undo",
+//     completed: false,
+//     id: 0
+//   },
+//   {
+//     text: "Implement Redo",
+//     completed: false,
+//     id: 1
+//   }
+// ];
+const initialState = {
+  past: [
+
+  ],
+  present: [
+    {
+      text: "Implement Undo",
+      completed: false,
+      id: 0
+    },
+    {
+      text: "Implement Redo",
+      completed: false,
+      id: 1
+    }
+  ],
+  future: [
+
+  ]
+}
+
+export default function todos(state = initialState, action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return {
+        ...state,
+        present: [
+          ...state.present,
+          {
+            id: state.present.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1,
+            completed: false,
+            text: action.text
+          }
+        ]
+      }
+
+    case DELETE_TODO:
+      return {
+        ...state,
+        present: state.present.filter((todo) => todo.id !== action.id)
+      }
+
+    case EDIT_TODO:
+      return {
+        ...state,
+        present: state.present.map((todo) =>
+          todo.id === action.id ? { ...todo, text: action.text } : todo
+        )
+      };
+
+    case COMPLETE_TODO:
+      return {
+        ...state,
+        present: state.present.map((todo) =>
+          todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
+        )
+      };
+
+    case COMPLETE_ALL_TODOS:
+      const areAllMarked = state.present.every((todo) => todo.completed);
+      return {
+        ...state,
+        present: state.present.map((todo) => ({
+          ...todo,
+          completed: !areAllMarked
+        }))
+      };
+
+    case CLEAR_COMPLETED:
+      return {
+        ...state,
+        present: state.present.filter((todo) => todo.completed === false)
+      }
+
+    default:
+      return state;
+  }
+}
---
+++ src/selectors/index.js
@@ -0,0 +1,34 @@
+import { createSelector } from 'reselect'
+import { SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE } from '../constants/TodoFilters'
+
+const getVisibilityFilter = state => state.visibilityFilter
+const getTodos = state => state.todos
+
+export const getVisibleTodos = createSelector(
+  [getVisibilityFilter, getTodos],
+  (visibilityFilter, todos) => {
+    switch (visibilityFilter) {
+      case SHOW_ALL:
+        return todos.present
+      case SHOW_COMPLETED:
+        return todos.present.filter(t => t.completed)
+      case SHOW_ACTIVE:
+        return todos.present.filter(t => !t.completed)
+      default:
+        throw new Error('Unknown filter: ' + visibilityFilter)
+    }
+  }
+)
+
+export const getCompletedTodoCount = createSelector(
+  [getTodos],
+  todos => {
+    console.log("AAA", todos);
+    return (
+      todos.present.reduce((count, todo) =>
+        todo.completed ? count + 1 : count,
+        0
+      )
+    )
+  }
+)
